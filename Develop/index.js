925b52b8e0469e5df9dc84b25c2f765e2c108a94
Dave <bbec0689@gmail.com>
2021-01-18 12:07:43 -0700

    created markdown

--- Develop/index.js
+++ Develop/index.js
@@ -105,8 +105,8 @@ const questions = [
         default: true
     },
     
-    
 ];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -14,8 +14,48 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
     return `# ${data.title}
 
+    # Project Title
+README
+
+## Description
+A README.md that was created by Node.js
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+Created with npm install inquirer
+
+## License
+
+MIT License
+
+## Usage
+
+Can be used to create a README for any project
+
+## Contributions
+
+Dave Becks
+
+## Tests
+
+index.js
+
+## Questions
+
+If there are any questions about this project please reach out at bbec0689@gmail.com or you can view my github account here: [bbec0689](https://github.com/bbec0689)
 
 `;
 }
 
 module.exports = generateMarkdown;
+console.log(generateMarkdown)
